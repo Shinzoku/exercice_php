b7e68ab93df62db35ef13d75939ff8fffb420267
Shinzoku <shinzoku62800@gmail.com>
2020-05-20 11:52:40 +0200

    sixième commit
     Faire 3 liste html imbriqués avec ces 3 tableaux (ul li)

--- exercice_php/index.php
+++ exercice_php/index.php
@@ -16,6 +16,27 @@
 //     echo '<div>' .$i*$i. '</div>';
 // }
 
-$a = 'bonjour ';
-$b = 'tout le monde';
-echo $a.$b;
+// $a = 'bonjour ';
+// $b = 'tout le monde';
+// echo $a.$b;
+
+$array1 = ["fruits" => ['pomme', 'poire', 'banane']];
+$array2 = ["vehicules" => ['voiture', 'motos', 'avion']];
+$array3 = ["pays" => ['italie', 'france', 'belgique']];
+
+echo'<ul><li>';
+foreach($array1 as $key => $val)
+{
+    echo $key . '<ul><li>' . $val[0] . '</li><li>' . $val[1] . '</li><li>' . $val[2] . '</li></ul>';
+}
+echo '</li><li>';
+foreach($array2 as $key => $val)
+{
+    echo $key . '<ul><li>' . $val[0] . '</li><li>' . $val[1] . '</li><li>' . $val[2] . '</li></ul>';
+}
+echo '</li><li>';
+foreach($array3 as $key => $val)
+{
+    echo $key . '<ul><li>' . $val[0] . '</li><li>' . $val[1] . '</li><li>' . $val[2] . '</li></ul>';
+}
+echo '</li></ul>';
