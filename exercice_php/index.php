766ec71f3adcf7f5aa8f9a65fde4284d4389f161
Shinzoku <shinzoku62800@gmail.com>
2020-05-20 13:46:23 +0200

    septième commit
    création du fichier function.php

--- exercice_php/index.php
+++ exercice_php/index.php
@@ -24,19 +24,36 @@ $array1 = ["fruits" => ['pomme', 'poire', 'banane']];
 $array2 = ["vehicules" => ['voiture', 'motos', 'avion']];
 $array3 = ["pays" => ['italie', 'france', 'belgique']];
 
-echo'<ul><li>';
-foreach($array1 as $key => $val)
-{
-    echo $key . '<ul><li>' . $val[0] . '</li><li>' . $val[1] . '</li><li>' . $val[2] . '</li></ul>';
-}
-echo '</li><li>';
-foreach($array2 as $key => $val)
-{
-    echo $key . '<ul><li>' . $val[0] . '</li><li>' . $val[1] . '</li><li>' . $val[2] . '</li></ul>';
-}
-echo '</li><li>';
-foreach($array3 as $key => $val)
-{
-    echo $key . '<ul><li>' . $val[0] . '</li><li>' . $val[1] . '</li><li>' . $val[2] . '</li></ul>';
-}
-echo '</li></ul>';
+// echo'<ul><li>';
+// foreach($array1 as $key => $val)
+// {
+//     echo $key . '<ul><li>' . $val[0] . '</li><li>' . $val[1] . '</li><li>' . $val[2] . '</li></ul>';
+// }
+// echo '</li><li>';
+// foreach($array2 as $key => $val)
+// {
+//     echo $key . '<ul><li>' . $val[0] . '</li><li>' . $val[1] . '</li><li>' . $val[2] . '</li></ul>';
+// }
+// echo '</li><li>';
+// foreach($array3 as $key => $val)
+// {
+//     echo $key . '<ul><li>' . $val[0] . '</li><li>' . $val[1] . '</li><li>' . $val[2] . '</li></ul>';
+// }
+// echo '</li></ul>';
+
+// // version plus performante
+// $arrayGlobal = array_merge($array1, $array2, $array3);
+
+// var_dump($arrayGlobal);
+
+// echo '<ul>';
+// foreach($arrayGlobal as $key => $array)
+// {
+//     echo '<li>'.$key . '<ul>';
+//     foreach($array as $value)
+//     {
+//         echo '<li>'.$value . '</li>';
+//     }
+//     echo '</ul></li>';
+// }
+
